80db5d9c1f53a70d586f8cb486d93079a066eaf9
Adam Kaufman <unmotivated@gmail.com>
2011-04-22 00:30:08 -0400

    clean up db calls a little. add nick recognition in playlist queueing

--- karaoke.py
+++ karaoke.py
@@ -7,23 +7,33 @@ SQLITE_DB='/tmp/example.db'
 CDG_DIR='/Volumes/Karaoke/'
 PYCDG_PATH='/Users/adam/Downloads/pykaraoke-0.7.3/'
 
-conn = sqlite3.connect(SQLITE_DB)
-c=conn.cursor().execute('''CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, filename TEXT UNIQUE)''')
-c=conn.cursor().execute('''CREATE TABLE IF NOT EXISTS playlist (id INTEGER PRIMARY KEY, song_id INTEGER, played BOOLEAN, timestamp TIMESTAMP, nick TEXT)''')
-conn.commit()
-conn.close()
+def create_db_tables():
+	conn = sqlite3.connect(SQLITE_DB)
+	c=conn.cursor().execute('''CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, filename TEXT UNIQUE)''')
+	c=conn.cursor().execute('''CREATE TABLE IF NOT EXISTS playlist (id INTEGER PRIMARY KEY, song_id INTEGER, played BOOLEAN, timestamp TIMESTAMP, nick TEXT)''')
+	conn.commit()
+	conn.close()
 
-def find(phenny, input):
-	sWords = re.sub("\.find\ ?", "", input).split(" ")
+def insert_song(fileName):
+	cn = sqlite3.connect(SQLITE_DB)
+	cn.text_factory = str
+	c = cn.cursor().execute("""INSERT OR IGNORE INTO songs (filename) VALUES (?);""", [fileName])
+	cn.commit()	
+	cn.close()
 
+def get_song(songID):
+	cn = sqlite3.connect(SQLITE_DB)
+	cn.text_factory = str
+	c = cn.cursor().execute("""SELECT filename, id FROM songs WHERE id = ?;""", [songID])
+	ret = c.fetchone()
+	cn.close()
+	return ret
+	
+def search_songs(sWords):
 	searchWords = []
 
 	for word in sWords:
 		searchWords.append("%" + word + "%")
-
-	if len(searchWords) < 1:
-		phenny.write(('PRIVMSG', input.nick), "Search query must be 3 or more characters")
-		return
 
 	query = "SELECT id, filename FROM songs WHERE "
 
@@ -39,48 +49,65 @@ def find(phenny, input):
 
 	cn = sqlite3.connect(SQLITE_DB)
 	cn.text_factory = str
+
 	c = cn.cursor().execute(query, searchWords)
+
+	results = []
+	for row in c.fetchall():
+		results.append([row[0], row[1]])
+
+	cn.close()
+
+	return results
+
+def queue_song(nick, songID):
+	cn = sqlite3.connect(SQLITE_DB)
+	cn.text_factory = str
+	now = datetime.datetime.now()
+	cn.cursor().execute("""INSERT INTO playlist ( timestamp, nick, song_id, played ) VALUES ( ?, ?, ?, ? )""", [ now, nick, songID, 0] )
+	cn.commit()
+	cn.close()
+
+def find(phenny, input):
+	sWords = re.sub("\.find\ ?", "", input).split(" ")
+
+	if len(sWords) < 1:
+		phenny.write(('PRIVMSG', input.nick), "Search query must be 3 or more characters")
+		return
 
 	retStr = "Results for search: " + (" ").join(sWords)
 	phenny.write(('PRIVMSG', input.nick), retStr)
 
-	for row in c:
+	for row in search_songs(sWords):
 		phenny.write(('PRIVMSG', input.nick), str(row[0]) + ": " + os.path.basename(str(row[1])))
 		time.sleep(.5)
 
-	cn.close()
+	phenny.write(('PRIVMSG', input.nick), "End Results")
 	
 find.commands = ['find']
 find.priority = 'medium'
 
+	
 def rebuild_cache(phenny, input):
 	files = get_cdg_files(CDG_DIR)
-	cn = sqlite3.connect(SQLITE_DB)
-	cn.text_factory = str
 	phenny.say("Updating cache for " + str(len(files)) + " files...")
+
 	for file in files:
-		c = cn.cursor().execute("""INSERT OR IGNORE INTO songs (filename) VALUES (?);""", [file])
-		cn.commit()	
-	cn.close()
+		insert_song(file)
+
+	phenny.say("Cache Updated!")
 
 rebuild_cache.commands = ['rebuild_cache']
 rebuild_cache.priority = 'medium'
 
+
 def play(phenny, input):
 	args = re.sub("\.play\ ?", "", input).split(" ")
-	cn = sqlite3.connect(SQLITE_DB)
-	cn.text_factory = str
-	c = cn.cursor().execute("""SELECT filename, id FROM songs WHERE id = ?;""", [args[0]])
-	res = c.fetchone()
-	playFile = str(res[0])
-	playId = str(res[1])
-
+	song = get_song(args[0])
+	playFile = str(song[0])
+	playId = str(song[1])
 	phenny.say("Queueing file: " + playFile)
-	now = datetime.datetime.now()
-	cn.cursor().execute("""INSERT INTO playlist ( timestamp, nick, song_id, played ) VALUES ( ?, ?, ?, ? )""", [ now, "adam[0]", playId, 0] )
-	cn.commit()
-	cn.close()
-	
+	queue_song(input.nick, playId)
 
 play.commands = ['play']
 play.priority = 'medium'
@@ -105,27 +132,40 @@ def get_cdg_files(dir):
 def setup(phenny): 
 	def monitor(phenny): 
 
-		cn = sqlite3.connect(SQLITE_DB)
 		time.sleep(5)
 		while True: 
-			c=cn.cursor().execute('''SELECT songs.filename, playlist.id FROM PLAYLIST INNER JOIN songs ON song_id = songs.id WHERE playlist.played = 0 ORDER BY timestamp LIMIT 1;''')
+			cn = sqlite3.connect(SQLITE_DB)
+			c=cn.cursor().execute('''SELECT songs.filename, playlist.id, playlist.nick FROM PLAYLIST INNER JOIN songs ON song_id = songs.id WHERE playlist.played = 0 ORDER BY timestamp LIMIT 1;''')
 			res = c.fetchone()
 			if (res != None):
 				cn.cursor().execute('''UPDATE playlist set played = 1 WHERE id = ?;''', [ res[1] ])
 				cn.commit()
-				phenny.msg("#hive76", "Playing: " + str(res[0]))
+
+				for channel in phenny.config.channels:
+					phenny.msg(channel, res[2] + " Will Now Be Singing " + str(res[0]))
+
 				playCMD = PYCDG_PATH + '/pycdg.py'
 				subprocess.call(["python", playCMD, "-w 800", "-h 600", res[0]])
-				phenny.msg("#hive76", "Done playing: " + str(res[0]))
+
+				for channel in phenny.config.channels:
+					phenny.msg(channel, "Done playing: " + str(res[0]))
+
 				time.sleep(10)
 
+			cn.close()
 			time.sleep(5)
 
 	targs = (phenny,)
 	t = threading.Thread(target=monitor, args=targs)
 	t.start()
 	
+def init():
+	create_db_tables()
+
+init()
 
 if __name__ == '__main__':
 	find(None, 'all')
+
+
 
